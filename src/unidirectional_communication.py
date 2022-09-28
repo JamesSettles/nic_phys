ae42ed22843334f0ac57c7e6bbe47439d60f1417
scural <s_cural@coloradocollege.edu>
2022-09-28 11:49:22 -0600

    added unidirectional code

---
+++ src/unidirectional_communication.py
@@ -0,0 +1,11 @@
+import nic_interface as nic
+
+nic.initialize_communication()
+
+input_val = input("are you receiving (r) or sending (s)")
+if(input_val == "r"):
+    nic.receive_message(1)
+elif(input_val == "s"):
+    while True:
+        input_msg = input("What message would you like to send")
+        nic.send_message(1,input_msg)
